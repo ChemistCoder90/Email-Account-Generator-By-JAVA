7b65de4fa243b4be4c5d1c40439d7e0e5971b795
chemistcoder90 <motaher.emon360@gmail.com>
2025-12-05 17:35:47 +0600

    Update accepct password change

--- EmailApp.java
+++ EmailApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class EmailApp{
   public static void main(String[] Args){
     Scanner scanner=new Scanner(System.in);
-    System.out.println("Please Enter ur Firstname,Lastname");
+    System.out.println("Please Enter ur Firstname, Lastname");
     String FirstName=scanner.nextLine();
     String LaststName=scanner.nextLine();
     email obj=new email(FirstName, LaststName);
--- email.java
+++ email.java
@@ -17,14 +17,19 @@ public class email {
     SetDept();
     emailid();
     Randompass(passlength);
-    // ChangePassword();
     EmployeeInfo();
+    boolean choice=true;
+    System.out.println("Do You want to Change Password?\n1. Yes\n2. No");
+    int x=input.nextInt();
+    if(x==1) ChangePassword();
+    else System.out.println("Have a Nice day.");
   }
   Scanner input = new Scanner(System.in);
 
   public void SetDept() {
-    System.out.println("Please Choose Department:");
+    System.out.println("Departments:");
     System.out.println("1. QC\n2. RnD\n3. Production\n4. None");
+    System.out.print("Choose Your Department: ");
     int choose= input.nextInt();
     if(choose==1) Department="QC";
     else if(choose==2) Department="Rnd";
@@ -47,10 +52,10 @@ public class email {
     Password=input.nextLine();
   }
   public void EmployeeInfo(){
-    System.out.println("Employee Name: "+FirstName+" "+LastName);
+    System.out.println("\nEmployee Name: "+FirstName+" "+LastName);
     System.out.println("Department Name: "+Department);
     System.out.println("Mail ID: "+emailID);
-    System.out.println("Mail Capacity: "+MailBoxCapacity);
-    System.out.println("Temporary Password: "+Password);
+    System.out.println("Mail Capacity: "+MailBoxCapacity+"mb");
+    System.out.println("Temporary Password: "+Password+"\n");
   }
 }
